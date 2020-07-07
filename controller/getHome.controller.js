25f761f497bee91cf8dc9612b31a763280e62c72
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2020-07-07 21:13:51 +0700

    load top four new famous in home page

--- controller/getHome.controller.js
+++ controller/getHome.controller.js
@@ -1,3 +1,11 @@
-module.exports.loadhome = function (req, res) {
-  res.render('index');
+const newsModel = require("../model/news.model");
+
+module.exports.loadhome = async function (req, res) {
+  const topFourNewsFamous = await newsModel.loadTopNewsFamous(4);
+  const topFourNewsFamousLength = topFourNewsFamous;
+
+  res.render("index", {
+    topFourNewsFamous,
+    topFourNewsFamousLength,
+  });
 };
