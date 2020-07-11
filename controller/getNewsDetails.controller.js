d510c00fb544b9e96501f00cdc5e9fff2512486c
TrangLuong95 <47876889+TrangLuong95@users.noreply.github.com>
2020-07-11 12:49:47 +0700

    update 5 bài viết liên quan trong trang NewsDetails

--- controller/getNewsDetails.controller.js
+++ controller/getNewsDetails.controller.js
@@ -1,15 +1,17 @@
 const newsModel = require("../model/news.model");
 
 module.exports.loadNewsDetails = async function (req, res) {
-    const id = req.params.id;
+  const id = req.params.id;
 
   const NewsDetails = await newsModel.singleNewsDetails(id);
   const TagOfNews = await newsModel.loadTagNews(id);
   const CmtNews = await newsModel.loadCmt(id);
+  const FiveRelatedPosts = await newsModel.loadFiveRelatedPosts(id, 5);
 
   res.render("vwNews/NewsDetails", {
     NewsDetails,
     TagOfNews,
     CmtNews,
+    FiveRelatedPosts,
   });
 };
