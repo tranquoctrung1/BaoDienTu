912fca66de116319c2682d41aabfc06f8db32601
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2020-07-12 19:02:47 +0700

    completed load data for home page

--- model/news.model.js
+++ model/news.model.js
@@ -9,3 +9,15 @@ module.exports.loadTopNewsFamous = function (quantity) {
     `SELECT n.NewsTitle, cc.CatChildName, c.CatName, n.DatePost, n.Avatar FROM ${TBL_NEWS} n join ${TBL_SUBCATEGORY} cc on cc.CatChild_ID = n.CatChild_ID join ${TBL_CATEGORY} c on c.CatID = cc.CatID ORDER BY n.Like DESC limit ${quantity} `
   );
 };
+
+module.exports.loadViewestNewsByCatId = function (id, quantity) {
+  return db.load(
+    `SELECT n.NewsTitle, cc.CatChildName, c.CatName, n.DatePost, n.Avatar FROM ${TBL_NEWS} n join ${TBL_SUBCATEGORY} cc on cc.CatChild_ID = n.CatChild_ID join ${TBL_CATEGORY} c on c.CatID = cc.CatID where c.CatID = '${id}' ORDER BY n.View DESC limit ${quantity}  `
+  );
+};
+
+module.exports.loadMostSoonNewsByCatId = function (id, quantity) {
+  return db.load(
+    `SELECT n.NewsTitle, cc.CatChildName, c.CatName, n.DatePost, n.Avatar FROM ${TBL_NEWS} n join ${TBL_SUBCATEGORY} cc on cc.CatChild_ID = n.CatChild_ID join ${TBL_CATEGORY} c on c.CatID = cc.CatID where c.CatID = '${id}' ORDER BY n.DatePost limit ${quantity}  `
+  );
+};
