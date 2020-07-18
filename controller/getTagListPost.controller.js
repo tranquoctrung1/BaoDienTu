eea491d33cd246489e650524604bb0e094b8ed66
Angel <ttien2384@gmail.com>
2020-07-18 17:43:22 +0700

    resolve conflict

--- controller/getTagListPost.controller.js
+++ controller/getTagListPost.controller.js
@@ -3,7 +3,7 @@ const tagListPost = require("../model/news.model");
 module.exports.loadTagList = async function (req, res) {
     const id = +req.params.id || 1;
 
-    const topFourNewsFamous = await tagListPost.loadTagListPost(id);
+    const topFourNewsFamous = await tagListPost.loadTagListPost(id,6,0);
     const topFourNewsFamousLength = topFourNewsFamous.length;
     console.log(topFourNewsFamous)
     
