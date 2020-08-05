bd7d41feedb64b803a0509b25b70257c8d722562
TrangLuong95 <47876889+TrangLuong95@users.noreply.github.com>
2020-08-05 08:00:20 +0700

    xử lý các trang nhỏ của Admin: quản lý chuyên mục, quản lý tag, quản lý bài viết, quản lý người dùng

--- model/admin.model.js
+++ model/admin.model.js
@@ -5,6 +5,8 @@ const TBL_NEWS = "news";
 const TBL_USER = "user";
 const TBL_TAG = 'tag';
 const TBL_TYPE_OF_USER = "type_of_user";
+const TBL_SUBCATEGORY = "category_child";
+const TBL_TAG_OF_NEWS = "tag_of_news";
 
 module.exports = {
     loadCat: function(){
@@ -19,4 +21,86 @@ module.exports = {
     loadUser: function(){
         return db.load(`SELECT * FROM ${TBL_USER} u JOIN ${TBL_TYPE_OF_USER} tou ON u.TypeOfUser = tou.TypeID`);
     },
+    addNewCategory: function(entity){
+        return db.add(TBL_CATEGORY, entity);
+    },
+    addNewTag: function(entity){
+        return db.add(TBL_TAG, entity);
+    },
+    loadCatChild: function(){
+        return db.load(`SELECT * FROM ${TBL_SUBCATEGORY}`);
+    },
+    addNewPost: function(entity){
+        return db.add(TBL_NEWS, entity);
+    },
+    loadTypeOfUser: function(){
+        return db.load(`SELECT * FROM ${TBL_TYPE_OF_USER}`);
+    },
+    addNewUser: function(entity){
+        return db.add(TBL_USER, entity);
+    },
+    loadUpdateCategory: function(CatID){
+        return db.load(`SELECT * FROM ${TBL_CATEGORY} c JOIN ${TBL_USER} u ON c.Manager = u.UserID WHERE CatID = ${CatID}`);
+    },
+    updateCategory: function (entity) {
+        const condition = {
+          CatID: entity.CatID
+        }
+        delete entity.CatID;
+        return db.patch(TBL_CATEGORY, entity, condition);
+    },
+    loadUpdateTag: function(TagID){
+        return db.load(`SELECT * FROM ${TBL_TAG} WHERE TagID = ${TagID}`);
+    },
+    updateTag: function (entity) {
+        const condition = {
+          TagID: entity.TagID
+        }
+        delete entity.TagID;
+        return db.patch(TBL_TAG, entity, condition);
+    },
+    loadUpdatePost: function(NewsID){
+        return db.load(`SELECT * FROM ${TBL_NEWS} n join ${TBL_USER} u on n.Author = u.UserID join ${TBL_SUBCATEGORY} cc on n.CatChild_ID = cc.CatChild_ID WHERE n.NewsID = ${NewsID}`);
+    },
+    updatePost: function (entity) {
+        const condition = {
+          NewsID: entity.NewsID
+        }
+        delete entity.NewsID;
+        return db.patch(TBL_NEWS, entity, condition);
+    },
+    loadCatChild: function(){
+        return db.load(`SELECT * FROM ${TBL_SUBCATEGORY}`);
+    },
+    delNews: function (id) {
+        const condition = {
+          NewsID: id
+        }
+        return db.del(TBL_NEWS, condition);
+    },
+    delTag_Of_News: function (id) {
+        const condition = {
+          NewsID: id
+        }
+        return db.del(TBL_TAG_OF_NEWS, condition);
+    },
+    updateAvatar: function(entity){
+        const condition = {
+          NewsID: entity.NewsID
+        }
+        delete entity.NewsID;
+        return db.patch(TBL_NEWS, entity, condition);
+    },
+    //====Quản lý User
+    loadUpdateUser: function(UserID){
+        return db.load(`SELECT * FROM ${TBL_USER} u JOIN ${TBL_TYPE_OF_USER} tou ON u.TypeOfUser = tou.TypeID WHERE UserID = ${UserID}`);
+    },
+    updateUser: function (entity) {
+        const condition = {
+            UserID: entity.UserID
+        }
+        delete entity.UserID;
+        return db.patch(TBL_USER, entity, condition);
+    },
+
 }
--- router/admin.route.js
+++ router/admin.route.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)},
+    destination(req, file, cb) {
+      cb(null, './public/images');
+    }
+  
+});
+const upload = multer({ storage });
 
 const getAdmin = require("../controller/getAdmin.controller");
 router.get('/', getAdmin.loadAdmin);
+
+router.get('/paddNewCategory', getAdmin.paddNewCategory);
+router.post('/paddNewCategory/addNewCategory', getAdmin.addCategory);
+router.get('/UpdateCategory/:id', getAdmin.loadUpdateCategory);
+router.post('/UpdateCategory/update', getAdmin.updateCategory);
+router.get('/Category_IsDel/:id', getAdmin.Category_IsDel);
+
+router.get('/paddNewTag', getAdmin.paddNewTag);
+router.post('/paddNewTag/addNewTag', getAdmin.addTag);
+router.get('/UpdateTag/:id', getAdmin.loadUpdateTag);
+router.post('/UpdateTag/update', getAdmin.updateTag);
+router.get('/Tag_IsDel/:id', getAdmin.Tag_IsDel);
+
+router.get('/paddNewPost', getAdmin.paddNewPost);
+router.post('/paddNewPost', upload.single("Avatar"), getAdmin.addNewPost);
+router.get('/UpdatePost/:id', getAdmin.loadUpdatePost);
+router.post('/UpdatePost/update', getAdmin.updatePost);
+router.post('/UpdatePost/updateAvatar', upload.single("Avatar"), getAdmin.updateAvatar);
+router.post('/UpdatePost/del', getAdmin.DelPost);
+router.get('/Post_IsDel/:id', getAdmin.Post_IsDel);
+
+router.get('/paddNewUser', getAdmin.paddNewUser);
+router.post('/paddNewUser/addNewUser', getAdmin.addUser);
+router.get('/UpdateUser/:id', getAdmin.loadUpdateUser);
+router.post('/UpdateUser/update', getAdmin.updateUser);
+router.get('/User_IsDel/:id', getAdmin.User_IsDel);
 
 module.exports = router;
