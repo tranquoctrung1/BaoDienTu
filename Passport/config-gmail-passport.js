ab7861358b5dca32f94da06669ed20bb41818e80
Quang Thie Sĩx <phamquangthien.it@gmail.com>
2020-08-10 11:40:57 +0700

    login fb gm middleware

--- Passport/config-gmail-passport.js
+++ Passport/config-gmail-passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const config = require("../config/config.json")
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const loginModel = require("../model/login.model");
+// const loginModel = require("../model/login.model");
 
 
 passport.serializeUser((user, done) => {
@@ -19,28 +19,8 @@ passport.use(new GoogleStrategy({
     },
     async function(accessToken, refreshToken, email, done) {
         // console.log(email);
-
-        var d = new Date(13, 7, 1995);
-        const entity = {
-                UserName: email.id,
-                Name: email.displayName,
-                Password: email.id,
-                Phone: '0909123123',
-                Email: email.emails[0].value,
-                TypeOfUser: 2,
-                Penname: email.displayName,
-                BirthDay: d,
-                IsActive: 1,
-                IsDel: 0,
-                token: email.id,
-                avata: email.photos[0].value
-            }
-            // console.log(email);
-
-
-        await loginModel.add(entity);
-
-        const abc = await loginModel.singleidgoogle(email.id);
-        done(null, abc);
+        process.nextTick(function() {
+            return done(null, email);
+        });
     }
 ));
