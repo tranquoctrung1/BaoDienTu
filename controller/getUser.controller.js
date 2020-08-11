da0a98545e10beb2163ec48326ed81dc5c98bcf3
Angel <ttien2384@gmail.com>
2020-08-11 21:24:11 +0700

    cap nhat thong tin

--- controller/getUser.controller.js
+++ controller/getUser.controller.js
@@ -7,12 +7,42 @@ module.exports.loadUser = async function (req, res){
 module.exports.loadUpdateUser = async function (req, res){
     const id = +req.params.id || 1;
 
-    res.render('vwUser/updateInfo');
+    const listUser = await UserModel.loadUpdateUser(id);
+
+    let isWriter = listUser[0].TypeOfUser === 3 ? true : false;
+
+    //console.log(listUser)
+
+    res.render('vwUser/updateInfo',{
+        listUser : listUser[0], 
+        isWriter
+    });
 };
 module.exports.updateUser = async function (req, res) {
-
-    console.log(req.body);
+    
+    let entity;
+    if(req.body.hasProperty('butdanh'))
+    {
+        entity = 
+        {
+            Name: req.body.UserName,
+            Email: req.body.Email,
+            BirthDay: req.body.bday,
+            PenName: req.body.butdanh,
+            UserID: req.body.id
+        }
+    }
+    else 
+    {
+        entity = 
+        {
+            Name: req.body.UserName,
+            Email: req.body.Email,
+            BirthDay: req.body.bday,
+            UserID: req.body.id
+        }
+    }
     await UserModel.updateUser(req.body);
   
-    //res.redirect("/User");
+    res.redirect("/User");
   }
