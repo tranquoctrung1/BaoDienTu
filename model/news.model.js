100ccf9421ada91ab877b6fe1e18fe5de103f447
Angel <ttien2384@gmail.com>
2020-08-12 19:21:02 +0700

    cap nhat pre Search, trang chu

--- model/news.model.js
+++ model/news.model.js
@@ -107,7 +107,7 @@ module.exports = {
   },
   getFullTextSearch: function (content, limit, offset) {
     return db.load(
-      `SELECT n.NewsTitle, cc.CatChildName, c.CatName,n.Abstract , n.DatePost, n.Avatar, c.CatID, cc.CatChild_ID, n.NewsID FROM ${TBL_NEWS} n join ${TBL_SUBCATEGORY} 
+      `SELECT n.NewsTitle, cc.CatChildName, c.CatName,n.Abstract , n.DatePost, n.Avatar, n.IsPremium, c.CatID, cc.CatChild_ID, n.NewsID FROM ${TBL_NEWS} n join ${TBL_SUBCATEGORY} 
       cc on cc.CatChild_ID = n.CatChild_ID join ${TBL_CATEGORY} c on c.CatID = cc.CatID 
       where n.isDel = 0 and (n.Status = 2 or n.Status = 1) and c.isDel = 0 and cc.isDel = 0 and match(n.NewsTitle, n.Abstract, n.Content) AGAINST ('${content}' IN NATURAL LANGUAGE MODE) limit ${limit} offset ${offset} `
     );
