b755771f54fcf76d3153078eb0b3b2ff26c6401e
TrangLuong95 <47876889+TrangLuong95@users.noreply.github.com>
2020-08-13 22:02:24 +0700

    fix Editor - truy van bai viet theo chuyen muc editor quan ly

--- model/editor.model.js
+++ model/editor.model.js
@@ -6,12 +6,21 @@ const TBL_NEWS = "news";
 const TBL_USER = "user";
 const TBL_TAG = 'tag';
 const TBL_TAG_OF_NEWS = "tag_of_news";
+const TBL_EDITOR_CAT = "editor_cat";
 
 module.exports = {
-    loadListPost_chuaduyet: function(CatID) {
-        return db.load(`SELECT * FROM ${TBL_NEWS} n JOIN ${TBL_SUBCATEGORY} cc ON n.CatChild_ID = cc.CatChild_ID JOIN ${TBL_CATEGORY} c ON cc.CatID = c.CatID JOIN ${TBL_USER} u ON n.Author = u.UserID WHERE Status = 4`);
+    // loadListPost_chuaduyet: function() {
+    //     return db.load(`SELECT * FROM ${TBL_NEWS} n JOIN ${TBL_SUBCATEGORY} cc ON n.CatChild_ID = cc.CatChild_ID JOIN ${TBL_CATEGORY} c ON cc.CatID = c.CatID JOIN ${TBL_USER} u ON n.Author = u.UserID WHERE Status = 4`);
+    //     // truy vấn chuyên mục do biên tập viên quản lý
+    //     // return db.load(`SELECT * FROM ${TBL_NEWS} n JOIN ${TBL_SUBCATEGORY} cc ON n.CatChild_ID = cc.CatChild_ID JOIN ${TBL_CATEGORY} c ON cc.CatID = c.CatID JOIN ${TBL_USER} u ON n.Author = u.UserID WHERE Status = 4 and c.CatID = ${CatID}`);
+    // },
+    loadListPost_chuaduyet: function(UserID) {
+        // truy vấn bài viết có chuyên mục do biên tập viên quản lý
+        return db.load(`SELECT n.NewsID, n.Avatar, n.NewsTitle, n.Abstract, n.DatePost, c.CatName FROM ${TBL_EDITOR_CAT} ec JOIN ${TBL_SUBCATEGORY} cc ON ec.CatID = cc.CatID JOIN ${TBL_CATEGORY} c ON cc.CatID = c.CatID JOIN ${TBL_NEWS} n ON cc.CatChild_ID = n.CatChild_ID JOIN ${TBL_USER} u ON n.Author = u.UserID WHERE ec.UserID = ${UserID} and n.Status = 4`);
+    },
+    loadList_CategoryEditorManager: function(UserID) {
         // truy vấn chuyên mục do biên tập viên quản lý
-        // return db.load(`SELECT * FROM ${TBL_NEWS} n JOIN ${TBL_SUBCATEGORY} cc ON n.CatChild_ID = cc.CatChild_ID JOIN ${TBL_CATEGORY} c ON cc.CatID = c.CatID JOIN ${TBL_USER} u ON n.Author = u.UserID WHERE Status = 4 and c.CatID = ${CatID}`);
+        return db.load(`SELECT ec.CatID, c.CatName FROM ${TBL_EDITOR_CAT} ec JOIN ${TBL_CATEGORY} c ON ec.CatID = c.CatID WHERE ec.UserID = ${UserID}`);
     },
     Review_loadNews: function(NewsID){
         return db.load(`SELECT n.NewsID, n.NewsTitle, u.Name, n.DatePost, n.View, n.Like, n.Abstract, n.Content, n.Avatar, cc.CatChildName from ${TBL_NEWS} n JOIN ${TBL_USER} u on n.Author = u.UserID JOIN ${TBL_SUBCATEGORY} cc ON n.CatChild_ID = cc.CatChild_ID JOIN ${TBL_CATEGORY} c ON cc.CatID = c.CatID WHERE NewsID = '${NewsID}'`);
